f470d4cade505c1bff891d9f8daaecdae5bc8ccd
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-05-13 16:49:43 -0400

    update

--- db/db-setup.js
+++ db/db-setup.js
@@ -5,6 +5,11 @@ const { Model } = require('objection');
 function setupDb() {
   // const db = knex(knexfile.development);
   const db = knex(knexfile.awsRDS);
+  console.log('execute db setup awsRDS', typeof nexfile.awsRDS)
+  console.log('execute db setup awsRDS connection', typeof nexfile.awsRDS.connection)
+  console.log('execute db setup awsRDS connection pass', typeof nexfile.awsRDS.connection.password)
+
+
   Model.knex(db);
 }
 
