46defd0f3b06eddeb719f86af458b867e829d017
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-05-13 16:57:01 -0400

    update syntax err

--- db/db-setup.js
+++ db/db-setup.js
@@ -5,9 +5,9 @@ const { Model } = require('objection');
 function setupDb() {
   // const db = knex(knexfile.development);
   const db = knex(knexfile.awsRDS);
-  console.log('execute db setup awsRDS', typeof nexfile.awsRDS)
-  console.log('execute db setup awsRDS connection', typeof nexfile.awsRDS.connection)
-  console.log('execute db setup awsRDS connection pass', typeof nexfile.awsRDS.connection.password)
+  console.log('execute db setup awsRDS', typeof knexfile.awsRDS)
+  console.log('execute db setup awsRDS connection', typeof knexfile.awsRDS.connection)
+  console.log('execute db setup awsRDS connection pass', typeof knexfile.awsRDS.connection.password)
 
 
   Model.knex(db);
