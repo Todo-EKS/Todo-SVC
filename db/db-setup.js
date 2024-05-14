9917c9773ed4bcb299a9f664b021b049cfc2bfbb
Vu Nguyen <nguyenanhhoanvu@gmail.com>
2024-05-13 20:52:17 -0400

    update code pipeline and code cleanup

--- db/db-setup.js
+++ db/db-setup.js
@@ -5,13 +5,6 @@ const { Model } = require('objection');
 function setupDb() {
   // const db = knex(knexfile.development);
   const db = knex(knexfile.awsRDS);
-  console.log('execute db setup awsRDS', typeof knexfile.awsRDS)
-  console.log('process.env.RDS_POSTGRES_DB_USERNAME ', process.env.RDS_POSTGRES_DB_USERNAME)
-  console.log('process.env.RDS_POSTGRES_DB_PASSWORD ', process.env.RDS_POSTGRES_DB_PASSWORD)
-  console.log('execute db setup awsRDS connection', typeof knexfile.awsRDS.connection)
-  console.log('execute db setup awsRDS connection pass', knexfile.awsRDS.connection.password)
-  console.log('execute db setup awsRDS connection host', knexfile.awsRDS.connection.host)
-  console.log('execute db setup awsRDS connection username', knexfile.awsRDS.connection.username)
 
   Model.knex(db);
 }
